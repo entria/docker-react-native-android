234df2bccfaa263a1fc7f7c0f48ac24dde81571c
Jonathan Cardoso <me@jonathancardoso.com>
2017-06-27 09:46:00 -0300

    Try to increase inotify

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sysctl fs.inotify.max_user_watches=524288
+echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches && echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_instances && watchman shutdown-server && sudo sysctl -p
 /bin/bash
